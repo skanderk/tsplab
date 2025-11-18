68f7f448f7d9a49733a2120c84c7473ccc0c2cc9
Skander Kort <skanderkort@gmail.com>
2025-11-18 08:30:29 -0500

    [i] Initial setup.

--- src/App.svelte
+++ src/App.svelte
@@ -16,7 +16,7 @@
   <h1>Vite + Svelte</h1>
 
   <div class="card">
-    <Counter />
+    <Counter/>
   </div>
 
   <p>
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{ts,js}'],
+  },
+  plugins: [svelte()]
+});
