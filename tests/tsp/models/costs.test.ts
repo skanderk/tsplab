b879b3df2d44a00370605a73cce2b6d74c26c938
Skander Kort <skanderkort@gmail.com>
2025-11-27 15:32:21 -0500

    [t] Removed tests folder from .gitignore.

---
+++ tests/tsp/models/costs.test.ts
@@ -0,0 +1,60 @@
+import { beforeEach, describe, it, expect } from "vitest";
+import { CostMatrix } from "../../../src/tsp/models/costs";
+import { InvalidNodeError, EmptyCostMatrixError } from "../../../src/tsp/models/error-types";
+
+describe("CostMatrix", () => {
+    let costMatrix: CostMatrix;
+
+    beforeEach(() => {
+        const costs = [[12, 23, 5], [85, 27], [99]];
+        costMatrix = new CostMatrix(costs);
+    });
+
+    describe("constructor()", () => {
+        it("throws when costs is an empty array", () => {
+
+
+            expect(() => new CostMatrix([])).toThrow(EmptyCostMatrixError);
+        })
+    });
+
+    describe("cost()", () => {
+        it("should return 0 when both node indexes are the same", () => {
+
+            const resultCost = costMatrix.cost(1, 1);
+
+            expect(resultCost).toEqual(0);
+        });
+
+        it("should return cost when first node index is less than second node index", () => {
+
+            const resultCost = costMatrix.cost(0, 2);
+
+            expect(resultCost).toEqual(23);
+        });
+
+        it("should return cost when first node index is greater than second node index", () => {
+
+            const resultCost = costMatrix.cost(3, 1);
+
+            expect(resultCost).toEqual(27);
+        });
+
+        it("should throw when called with node indexes out of bounds", () => {
+
+
+            expect(() => costMatrix.cost(-5, 2)).toThrow(InvalidNodeError);
+            expect(() => costMatrix.cost(1, 12)).toThrow(InvalidNodeError);
+            expect(() => costMatrix.cost(1, 4)).toThrow(InvalidNodeError);
+        })
+    });
+
+    describe("order()", () => {
+        it("should return the number of rows (columns) of the full square matrix", () => {
+
+            const resultOrder = costMatrix.order();
+
+            expect(resultOrder).toEqual(4);
+        })
+    })
+});
---
+++ tests/tsp/models/move-types.test.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from "vitest";
+import { Edge } from "../../../src/tsp/models/graph-types";
+import { EdgeExchange } from "../../../src/tsp/models/move-types";
+import { InvalidEdgeExchangeError } from "../../../src/tsp/models/error-types";
+
+describe("EdgeExchange", () => {
+    describe("constructor()", () => {
+        it("throws when the number of edges to remove and the number of edges to insert do not match", () => {
+            const removals = new Set([new Edge(1, 5), new Edge(10, 15)]);
+            const insertions = new Set([new Edge(2, 7), new Edge(56, 21), new Edge(7, 9)]);
+
+            expect(() => new EdgeExchange(removals, insertions)).toThrow(InvalidEdgeExchangeError)
+        })
+    });
+})
